6f7e09723c4f3ccc4310b6594e8e5de67c84f9d4
Viviane Chan <vivianechan@Vivianes-MacBook-Pro.local>
2017-01-12 21:05:38 -0500

    first commit

---
+++ Pods/Target Support Files/Pods-Dockmaster/Pods-Dockmaster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dockmaster : NSObject
+@end
+@implementation PodsDummy_Pods_Dockmaster
+@end
---
+++ Pods/Target Support Files/Pods-DockmasterTests/Pods-DockmasterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DockmasterTests : NSObject
+@end
+@implementation PodsDummy_Pods_DockmasterTests
+@end
