226a2cb1bf715b24908161c1a6ff5dfe4ac26d26
Viviane Chan <vivianechan@Vivianes-MacBook-Pro.local>
2017-01-12 21:54:57 -0500

    Login Screen. WIP

---
+++ DerivedData/Dockmaster/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-DockmasterUITests.build/DerivedSources/Pods_DockmasterUITests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_DockmasterUITestsVersionString[];
+ extern const double Pods_DockmasterUITestsVersionNumber;
+
+ const unsigned char Pods_DockmasterUITestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_DockmasterUITests  PROJECT:Pods-1" "\n";
+ const double Pods_DockmasterUITestsVersionNumber __attribute__ ((used)) = (double)1.;
